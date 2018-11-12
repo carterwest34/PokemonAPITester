0986bc2be2d4114976303308dda1b5472212af5f
Carter West <carterwest@MacBook-Pro-67.local>
2018-11-12 10:40:46 -0600

    Initialized pokemon api tester

---
+++ Pokèmon/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Pokèmon
+//
+//  Created by Carter West on 11/12/18.
+//  Copyright © 2018 Carter West. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var pokemonTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func searchButtonTapped(_ sender: Any) {
+    
+    }
+}
+
